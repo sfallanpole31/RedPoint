c645b541b3f5e56992b6a7bfa10c349d338b6980
Allan <hallan94093@hotmail.com>
2025-01-09 15:53:20 +0800

    撿紅點 同步

--- Assets/Scripts/卡牌/TurnManager.cs
+++ Assets/Scripts/卡牌/TurnManager.cs
@@ -6,8 +6,8 @@ public class TurnManager : MonoBehaviour
 {
 
     [SerializeField] int currentPlayerIndex = 0; // 當前回合的玩家索引
-    private bool hasPlayedCard = false; // 當前玩家是否已出牌
-    private bool hasDrawnCard = false;  // 當前玩家是否已抽牌
+    //private bool hasPlayedCard = false; // 當前玩家是否已出牌
+    //private bool hasDrawnCard = false;  // 當前玩家是否已抽牌
 
     DeckManager deckManager;
     CardSelector cardSelector;
@@ -43,8 +43,8 @@ public class TurnManager : MonoBehaviour
         // 結束當前玩家回合並輪到下一位玩家
         cardSelector.IsAction = false;
         cardSelector.isActivePlayer = false;
-        hasPlayedCard = false;
-        hasDrawnCard = false;
+        //hasPlayedCard = false;
+        //hasDrawnCard = false;
         currentPlayerIndex = (currentPlayerIndex + 1) % 4;
         Debug.Log("輪到玩家：" + currentPlayerIndex);
         deckManager.Players[currentPlayerIndex].GetComponent<CardSelector>().isActivePlayer = true;
